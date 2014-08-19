28bc4f0cfe3e736788321cd269036e805f837e28
lishiyo <c.ziye.li@gmail.com>
2014-08-19 02:16:04 -0400

    latest version 1.1.3, testing to see if it works as config was broken in 1.1.2

--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-c:\Users\Shiyo\Google Drive\(CODE)\(SEEDS)\CHAFFY\platforms\android\ant-build\AndroidManifest.xml : \
-c:\Users\Shiyo\Google Drive\(CODE)\(SEEDS)\CHAFFY\platforms\android\AndroidManifest.xml \
+C:\Users\Shiyo\Google Drive\(CODE)\(SEEDS)\CHAFFY-ARM\platforms\android\ant-build\AndroidManifest.xml : \
+C:\Users\Shiyo\Google Drive\(CODE)\(SEEDS)\CHAFFY-ARM\platforms\android\AndroidManifest.xml \
